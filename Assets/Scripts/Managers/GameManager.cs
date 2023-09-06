c1b6f5a3e2db1bcf14f5cee9ab269821c3f40f36
BAE KYUNGTAEK <125863048+GTAEKI@users.noreply.github.com>
2023-09-06 14:31:18 +0900

    feat : Creat MagneticField Prefab
    자기장 구현을 위한 Sphere 구현완료

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 게임매니저
+/// 배경택_230906
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    // 게임매니저 싱글톤_230906 배경택
+    public static GameManager instance;
+
+    // 카메라_230906 배경택
+    public Camera cameraSafeArea;
+    public Camera cameraBattle1;
+    public Camera cameraBattle2;
+
+    private void Awake()
+    {
+        //게임매니저 싱글톤_230906 배경택
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    //TODO 타이머
+    //TODO 자기장
+    //TODO 오브젝트 생성
+    //TODO 게임스테이지 흐름제어
+    //TODO 카메라 제어
+    //TODO 승패 판정
+    //TODO 팀 체력 제어
+
+    #region 카메라 이동
+    public void MoveToBattle1()
+    {
+        cameraSafeArea.gameObject.SetActive(false);
+        cameraBattle2.gameObject.SetActive(false); ;
+        cameraBattle1.gameObject.SetActive(true);
+    }
+
+    public void MoveToBattle2()
+    {
+        cameraSafeArea.gameObject.SetActive(false);
+        cameraBattle1.gameObject.SetActive(false);
+        cameraBattle2.gameObject.SetActive(true);
+    }
+
+    public void ReturnSafeZone()
+    {
+        cameraBattle1.gameObject.SetActive(false);
+        cameraBattle2.gameObject.SetActive(false);
+        cameraSafeArea.gameObject.SetActive(true);
+    }
+    #endregion
+}
