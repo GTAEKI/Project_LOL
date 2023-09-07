31333e035c1a56fd4bf57523bab13bf3b78c8673
MSKim0215 <rlaalstjqu05@gmail.com>
2023-09-07 12:02:34 +0900

    refactor: change to core managers
    1) 코어 매니저 코드 수정 및 개선

--- Assets/Scripts/Managers/PoolManager.cs
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -4,67 +4,88 @@ using UnityEngine;
 
 public class PoolManager
 {
-    #region Pool
     private class Pool
     {
-        public GameObject Original { get; private set; }
-        public Transform Root { get; set; }
+        public GameObject Original { private set; get; }
+        public Transform Root { set; get; }
 
-        private Stack<Poolable> poolStack = new Stack<Poolable>();
+        Queue<Poolable> poolQueue = new Queue<Poolable>();
 
+        /// <summary>
+        /// 풀 초기화 함수
+        /// </summary>
+        /// <param name="_original">원본 오브젝트</param>
+        /// <param name="_count">풀 생성시 초기 생성 개수</param>
         public void Init(GameObject _original, int _count = 5)
         {
             Original = _original;
             Root = new GameObject().transform;
             Root.name = $"{Original.name}_Root";
 
-            for(int i = 0; i < _count; i++)
+            for (int i = 0; i < _count; i++)
             {
                 Push(Create());
             }
         }
 
+        /// <summary>
+        /// 풀 오브젝트 생성함수
+        /// </summary>
+        /// <returns></returns>
         private Poolable Create()
         {
-            GameObject clone = Object.Instantiate(Original);
-            clone.name = Original.name;
-            return clone.GetOrAddComponent<Poolable>();
+            GameObject prefab = Object.Instantiate<GameObject>(Original);
+            prefab.name = Original.name;
+            return prefab.GetOrAddComponent<Poolable>();
         }
 
-        public void Push(Poolable _poolable)
+        /// <summary>
+        /// 풀 오브젝트 초기화 함수
+        /// </summary>
+        /// <param name="_able">풀 오브젝트</param>
+        public void Push(Poolable _able)
         {
-            if (_poolable == null) return;
+            if (_able == null) return;
 
-            _poolable.transform.parent = Root;
-            _poolable.gameObject.SetActive(false);
-            _poolable.isUsing = false;
+            _able.transform.SetParent(Root);
+            _able.gameObject.SetActive(false);
+            _able.isUsing = false;
 
-            poolStack.Push(_poolable);
+            poolQueue.Enqueue(_able);
         }
 
+        /// <summary>
+        /// 풀 오브젝트 사용하는 함수
+        /// </summary>
+        /// <param name="_parent">부모</param>
+        /// <returns></returns>
         public Poolable Pop(Transform _parent)
         {
-            Poolable poolable = null;
+            Poolable able;
 
-            if (poolStack.Count > 0) poolable = poolStack.Pop();
-            else poolable = Create();
-
-            poolable.gameObject.SetActive(true);
-
-            if(_parent == null)
-            {   // DontDestroyOnLoad 해제 용도
-                poolable.transform.parent = Managers.Scene.CurrentScene.transform;
+            if(poolQueue.Count > 0)
+            {
+                able = poolQueue.Dequeue();
+            }
+            else
+            {
+                able = Create();
             }
 
-            poolable.transform.parent = _parent;
-            poolable.isUsing = true;
+            able.gameObject.SetActive(true);
 
-            return poolable;
+            if(_parent == null)
+            {   // TODO: DontDestroyOnLoad 해제
+                able.transform.parent = Managers.Scene.CurrentScene.transform;
+            }
+
+            able.transform.SetParent(_parent);
+            able.isUsing = true;
+            return able;
         }
     }
-    #endregion
 
-    private Dictionary<string, Pool> poolDict= new Dictionary<string, Pool>();
+    private Dictionary<string, Pool> pools = new Dictionary<string, Pool>();
     private Transform root;
 
     public void Init()
@@ -76,49 +97,53 @@ public class PoolManager
         }
     }
 
+    /// <summary>
+    /// 풀에 집어넣는 함수
+    /// </summary>
+    /// <param name="_able">해당 객체</param>
+    public void Push(Poolable _able)
+    {
+        string name = _able.name;
+        if (!pools.ContainsKey(name)) GameObject.Destroy(_able.gameObject);
+        pools[name].Push(_able);
+    }
+
+    /// <summary>
+    /// 풀에서 꺼내서 사용하는 함수
+    /// </summary>
+    /// <param name="_original">원본 오브젝트</param>
+    /// <param name="_parent">해당 오브젝트의 부모</param>
+    public Poolable Pop(GameObject _original, Transform _parent = null)
+    {
+        if(!pools.ContainsKey(_original.name)) CreatePool(_original);
+        return pools[_original.name].Pop(_parent);
+    }
+
     public void CreatePool(GameObject _original, int _count = 5)
     {
         Pool pool = new Pool();
         pool.Init(_original, _count);
-        pool.Root.parent = root.transform;
-
-        poolDict.Add(_original.name, pool);
+        pool.Root.SetParent(root);
+        pools.Add(_original.name, pool);
     }
 
-    public void Push(Poolable _poolable)
-    {
-        string name = _poolable.gameObject.name;
-        if(!poolDict.ContainsKey(name))
-        {
-            Object.Destroy(_poolable.gameObject);
-            return;
-        }
-
-        poolDict[name].Push(_poolable);
-    }
-
-    public Poolable Pop(GameObject _original, Transform _parent = null)
-    {
-        if(!poolDict.ContainsKey(_original.name))
-        {
-            CreatePool(_original);
-        }
-
-        return poolDict[_original.name].Pop(_parent);
-    }
-
+    /// <summary>
+    /// 원본 오브젝트 리턴 함수
+    /// </summary>
+    /// <param name="_name">오브젝트 이름</param>
+    /// <returns></returns>
     public GameObject GetOriginal(string _name)
     {
-        if (!poolDict.ContainsKey(_name)) return null;
-        return poolDict[_name].Original;
+        if (!pools.ContainsKey(_name)) return null;
+        return pools[_name].Original;
     }
 
     public void Clear()
     {
         foreach(Transform child in root)
         {
-            Object.Destroy(child.gameObject);
+            GameObject.Destroy(child.gameObject);
         }
-        poolDict.Clear();
+        pools.Clear();
     }
 }
