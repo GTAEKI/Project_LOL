93dfbd81b117dfc8499ca9c2054a09f2ec4c00ab
DESKTOP-5S5CCIF\KGA_09 <pjk2155@naver.com>
2023-09-07 10:36:06 +0900

    Feat.strengthcard
    증강체 리롤 작업

--- Assets/JungKeun/Script/Card.cs
+++ Assets/JungKeun/Script/Card.cs
@@ -8,18 +8,65 @@ using UnityEngine.UI;
 public class Card : MonoBehaviour
 {
     public GameObject[] card;
-    
+    public GameObject RetryNumber;
 
     public Sprite[] strengthcard;
 
+    // 기존 카드
+    public List<int> cardNum = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    private int chance = 0;
 
 
     public void reroll()
     {
-        for(int a = 0; a<3; a++)
+        if (chance == 0)
         {
-            int RandomNumber = Random.Range(0, 5);
-            card[a].GetComponent<Image>().sprite = strengthcard[RandomNumber];
+            // 스왑을 500번 돌려
+            for (int i = 0; i < 500; i++)
+            {
+                Swap();
+            }
+
+            for (int a = 0; a < 3; a++)
+            {
+                card[a].GetComponent<Image>().sprite = strengthcard[cardNum[a]];
+            }
+            RetryNumber.GetComponent<Text>().text = "2";
+            chance++;
         }
+
+        else if (chance == 1)
+        {
+            for (int a = 0; a < 3; a++)
+            {
+                card[a].GetComponent<Image>().sprite = strengthcard[cardNum[a + 3]];
+            }
+            RetryNumber.GetComponent<Text>().text = "1";
+            chance++;
+        }
+
+        else if (chance == 2)
+        {
+            for (int a = 0; a < 3; a++)
+            {
+                card[a].GetComponent<Image>().sprite = strengthcard[cardNum[a + 6]];
+            }
+            RetryNumber.GetComponent<Text>().text = "0";
+            chance++;
+        }
+
+        else { return; }
+    }
+
+    // 스왑으로 짜
+    public void Swap()
+    {
+        int ranIndex1 = Random.Range(0, 10);
+        int ranIndex2 = Random.Range(0, 10);
+
+        int _temp = cardNum[ranIndex1];
+        cardNum[ranIndex1] = cardNum[ranIndex2];
+        cardNum[ranIndex2] = _temp;
     }
 }
