11daa66787cdbac89b3a798df09b4728715e12a9
nsbsb99 <solbin9910@gmail.com>
2023-09-20 17:58:09 +0900

    Feat Caityln Q Skill
    케이틀린 Q키 대략 완성

--- Assets/Solbin/SB_Scripts/Player/Caityln/SB_CaitylnQ.cs
+++ Assets/Solbin/SB_Scripts/Player/Caityln/SB_CaitylnQ.cs
@@ -1,20 +1,110 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class SB_CaitylnQ : MonoBehaviour
 {
+    Transform caityln;
+
     Animator caitylnAnimator; // 캐릭터 
     Animator armorAnimator; // 총알
 
+    GameObject qAttackPrefab; // q스킬 프리팹
+    GameObject qAttack; // q스킬 총알
+
+    bool isAttack = false;
+    bool bulletFire = false; // 총알 발사
+
+    Camera camera;
+    Vector3 targetPosition; // 공격이 향할 마우스 포지션
+
     private void Start()
     {
-        caitylnAnimator = transform.parent.GetComponent<Animator>();
-        armorAnimator = transform.GetComponent<Animator>();
+        caityln = transform.parent;
+        caitylnAnimator = caityln.GetComponent<Animator>();
+
+        qAttackPrefab = (GameObject)AssetDatabase.LoadAssetAtPath
+            ("Assets/Solbin/SB_Prefabs/Attack_Q.prefab", typeof(GameObject));
+
+        qAttack = Instantiate(qAttackPrefab);
+        qAttack.transform.SetParent(caityln);
+        qAttack.transform.position = new Vector3(caityln.position.x, 2.5f, caityln.position.z);
+
+        armorAnimator = qAttack.transform.GetComponent<Animator>();
+
+        camera = GameObject.Find("GameView Camera").GetComponent<Camera>();
     }
 
     public void SkillQ()
     {
-        Debug.Log("확인 완료");
+        if (!isAttack)
+        {
+            int layerMask = 1 << LayerMask.NameToLayer("Floor"); 
+
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+            {
+                targetPosition = hit.point;
+            }
+
+            StartCoroutine(Fire());
+        }
     }
+
+    private IEnumerator Fire()
+    {
+
+        isAttack = true; // 중복 키 입력 X
+
+        caitylnAnimator.SetTrigger("PressQ");
+        
+        Vector3 dir = targetPosition - caityln.position;
+        dir.y = 0f;
+
+        Quaternion targetRotation = Quaternion.LookRotation(dir); // 목표 방향
+        caityln.transform.rotation = targetRotation;
+
+        qAttack.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        bulletFire = true; // 총알 발사
+        Invoke("UnfoldBullet", 0.5f); // 총알 펼치기 (애니)
+
+        yield return new WaitForSeconds(caitylnAnimator.GetCurrentAnimatorClipInfo(0).Length);
+        caitylnAnimator.SetBool("PressQ_Idle", true);
+        yield return new WaitForSeconds(caitylnAnimator.GetCurrentAnimatorClipInfo(0).Length);
+        caitylnAnimator.SetBool("PressQ_Idle", false);
+
+        isAttack = false;
+    }
+
+    private void UnfoldBullet()
+    {
+        armorAnimator.enabled = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (bulletFire)
+        {
+            qAttack.transform.Translate(targetPosition * Time.deltaTime * 0.5f);
+
+            if (Vector3.Distance(qAttack.transform.position, caityln.position) > 15f)
+            {
+                bulletFire = false;
+
+                armorAnimator.Rebind(); // 총알 애니메이션 되감기
+                armorAnimator.enabled = false;
+
+                qAttack.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+                qAttack.transform.position = new Vector3(caityln.position.x, 2.5f, caityln.position.z);
+            }
+        }
+        
+    }
+
+
 }
