9c3bda080f6d9a934d32dbe40a9895167161323c
nsbsb99 <solbin9910@gmail.com>
2023-09-26 16:01:40 +0900

    Feat Gragas
    그라가스 자동 평타 추가 중, Q 마우스 위치로 수정 필요

--- Assets/Solbin/SB_Scripts/Player/Gragas/SB_GragasE.cs
+++ Assets/Solbin/SB_Scripts/Player/Gragas/SB_GragasE.cs
@@ -12,6 +12,12 @@ public class SB_GragasE : MonoBehaviour
     Vector3 gragasDes;
 
     bool isAttack = false;
+    bool apply = false;
+
+    private List<Rigidbody> rbList = new List<Rigidbody>();
+    private List<Vector3> dirList = new List<Vector3>();
+    private List<Vector3> originPosList = new List<Vector3>();
+    private List<GameObject> targetList = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -51,6 +57,76 @@ public class SB_GragasE : MonoBehaviour
         SB_GragasMoving.gragasSkill = false;
     }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && isAttack && other.name !="Gragas")
+        {
+            Rigidbody playerRigidbody = other.GetComponent<Rigidbody>();
+
+            Vector3 originPos = other.gameObject.transform.position;
+            Vector3 gragasPos = transform.position;
+            gragasPos.y = originPos.y;
+            Vector3 dir = other.gameObject.transform.position - gragasPos;
+            dir = dir.normalized;
+            GameObject target = other.gameObject;
+
+            if (playerRigidbody != null)
+            {
+                originPosList.Add(originPos);
+                rbList.Add(playerRigidbody);
+                dirList.Add(dir);
+                targetList.Add(target);
+
+                apply = true;
+
+                KnockBack();
+            }
+        }
+
+        if (other.name == "Dummy" && isAttack && other.name != "Gragas")
+        {
+            Rigidbody playerRigidbody = other.GetComponent<Rigidbody>();
+
+            Vector3 originPos = other.gameObject.transform.position;
+            Vector3 gragasPos = transform.position;
+            gragasPos.y = originPos.y;
+            Vector3 dir = other.gameObject.transform.position - gragasPos;
+            dir = dir.normalized;
+            GameObject target = other.gameObject;
+
+            if (playerRigidbody != null)
+            {
+                originPosList.Add(originPos);
+                rbList.Add(playerRigidbody);
+                dirList.Add(dir);
+                targetList.Add(target);
+
+                apply = true;
+
+                KnockBack();
+            }
+        }
+    }
+
+    private void KnockBack()
+    {
+        for (int i = 0; i < rbList.Count; i++)
+        {
+            rbList[i].velocity = dirList[i] * 10f;
+        }
+
+        ApplyDamage();
+    }
+
+    private void ApplyDamage()
+    {
+        for (int i = 0; i < rbList.Count; i++)
+        {
+            targetList[i].GetComponent<Unit>().CurrentUnitStat.OnDamaged(10f);
+        }
+    }
+
     private void FixedUpdate()
     {
         if (isAttack)
@@ -61,7 +137,19 @@ public class SB_GragasE : MonoBehaviour
             {
                 isAttack = false;
                 animator.SetTrigger("Back Idle");
+            } 
+        }
+
+        if (apply)
+        {
+            for (int i = 0; i < rbList.Count; i++)
+            {
+                if (Vector3.Distance(originPosList[i], targetList[i].transform.position) > 6f) // 6 초과 넉백되면
+                {
+                    rbList[i].velocity = Vector3.zero; // 넉백 종료
+                    apply = false;
+                }
             }
         }
-    }
+    }   
 }
